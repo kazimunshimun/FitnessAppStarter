b06a6c0da9e30345d071705d3144be0c3a4fea14
kazimunshimun <anik1.bd@gmail.com>
2025-07-11 23:51:12 +0200

    Updates home view

--- FitnessAppStarter/FitnessAppStarter/App/FitnessAppStarterApp.swift
+++ FitnessAppStarter/FitnessAppStarter/App/FitnessAppStarterApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FitnessAppStarterApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
--- FitnessAppStarter/FitnessAppStarter/Features/Home/HomeView.swift
+++ FitnessAppStarter/FitnessAppStarter/Features/Home/HomeView.swift
@@ -5,3 +5,16 @@
 //  Created by Kazi Munshimun Nabi on 11/7/25.
 //
 
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Home View!")
+        }
+        .padding()
+    }
+}
