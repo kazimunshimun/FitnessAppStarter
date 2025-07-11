8ade22700f3b6b1b0be41e3ae425167b0983e442
kazimunshimun <anik1.bd@gmail.com>
2025-07-11 22:07:56 +0200

    Initial Commit

---
+++ FitnessAppStarter/FitnessAppStarter/FitnessAppStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppStarterApp.swift
+//  FitnessAppStarter
+//
+//  Created by Kazi Munshimun Nabi on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
