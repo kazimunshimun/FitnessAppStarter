812380e679e32a9396247a327f809376d084e34c
kazimunshimun <anik1.bd@gmail.com>
2025-07-12 00:02:13 +0200

    Adds data folder

---
+++ FitnessAppStarter/FitnessAppStarter/Data/Models/WorkoutRoutine.swift
@@ -0,0 +1,7 @@
+//
+//  WorkoutRoutine.swift
+//  FitnessAppStarter
+//
+//  Created by Kazi Munshimun Nabi on 11/7/25.
+//
+
---
+++ FitnessAppStarter/FitnessAppStarter/Data/Services/WorkoutDataService.swift
@@ -0,0 +1,7 @@
+//
+//  WorkoutDataService.swift
+//  FitnessAppStarter
+//
+//  Created by Kazi Munshimun Nabi on 11/7/25.
+//
+
